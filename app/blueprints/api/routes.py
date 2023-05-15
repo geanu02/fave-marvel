c091d83e69204b3c19dcc4908f8813c1bb68922e
Gian Carlo Torres <geanu@outlook.com>
2023-05-15 11:30:46 -0400

    api/routes fixes

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -16,35 +16,17 @@ def marvel_all(user):
     marvel = Marvel.query.all()
     if marvel:
         result = marvels_schema.dump(marvel)
-        return jsonify(result)
-    # if marvel:
-    #     result = []
-    #     for m in marvel:
-    #         result.append({
-    #             'id': m.id,
-    #             'marvel_id': m.marvel_id,
-    #             'marvel_name': m.m_name,
-    #             'marvel_desc': m.m_desc,
-    #             'marvel_img': m.m_img,
-    #             'marvel_comics': m.m_comics
-    #         })
-    #     return make_response(jsonify(result), 200)
-    # else:
-    #     return jsonify([{'message':'No characters available to view.'}]), 404
+        return jsonify(result), 200
+    return jsonify([{'message':'No characters available to view.'}]), 404
 
 # Receive Marvel Character by marvel_id
 @bp.get('/marvel/<marvel_id>')
 @token_required
 def marvel_single_id(user, marvel_id):
-    m = Marvel.query.filter_by(marvel_id=marvel_id).first()
-    if m:
-        return jsonify([{
-            'id': m.id,
-            'marvel_id': m.marvel_id,
-            'marvel_name': m.m_name,
-            'marvel_img': m.m_img,
-            'marvel_comics': m.m_comics
-        }]), 200
+    marvel = Marvel.query.filter_by(marvel_id=marvel_id).first()
+    if marvel:
+        result = marvels_schema.dump(marvel)
+        return jsonify(result), 200
     return jsonify([{'message':'No characters available to view.'}]), 404
 
 # Receive All Favorite Marvel Characters from User by username
@@ -66,7 +48,7 @@ def marvel_all_by_user(user, username):
                 'marvel_img': m.m_img,
                 'marvel_comics': m.m_comics
             } for f, m in join_fave_marvel]), 200
-        return jsonify([{'message' : '{username} does not have favorites.'}]), 404
+        return jsonify([{'message' : f'{username} does not have favorites.'}]), 404
     return jsonify([{'message' : 'Invalid username.'}]), 404
 
 # Receive Favorite Marvel Character by marvel_id from User by username
@@ -89,6 +71,6 @@ def marvel_single_id_by_user(user, username, marvel_id):
                     'marvel_img': m.m_img,
                     'marvel_comics': m.m_comics
                 } for f, m in join_fave_marvel]), 200
-            return jsonify([{'message' : '{username} does not have favorites.'}]), 404
-        return jsonify([{'message' : '{marvel_id} not registered.'}]), 404
+            return jsonify([{'message' : f"{marvel_id} is not one of {username}'s favorite."}]), 404
+        return jsonify([{'message' : f'{marvel_id} not registered.'}]), 404
     return jsonify([{'message' : 'Invalid username.'}]), 404
